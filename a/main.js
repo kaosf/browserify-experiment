de7948dae44f56f8638bc29c961911e9fd3aa503
ka <ka.kaosf@gmail.com>
2014-12-25 02:22:32 +0900

    Add notes for gamma function

--- a/main.js
+++ a/main.js
@@ -3,5 +3,9 @@ var bar = require('../lib/bar.js');
 var gamma = require('gamma');
 
 var x = foo(1) + bar('baz');
+// foo(1) == 3
+// bar('baz') == 1
+// x == 4
+// gamma(n + 1) == n!
 
 console.log(gamma(x));
