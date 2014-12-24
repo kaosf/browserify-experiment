5433b7ccbe8cbad22464686f3c0c2bc50463aaa0
ka <ka.kaosf@gmail.com>
2014-12-25 02:13:49 +0900

    Modify to use foo.js and bar.js

--- a/main.js
+++ a/main.js
@@ -2,6 +2,6 @@ var foo = require('./foo.js');
 var bar = require('../lib/bar.js');
 var gamma = require('gamma');
 
-var elem = document.getElementById('result');
-var x = foo(100) + bar('baz');
-elem.textContent = gamma(x);
+var x = foo(1) + bar('baz');
+
+console.log(gamma(x));
--- a/run.sh
+++ a/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-touch ./foo.js
-mkdir ../lib
-touch ../lib/bar.js
 npm install gamma
 browserify main.js > bundle.js
